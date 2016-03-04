2b984d3e7b6e235fcee1a169c54bc86738ae2a39
Mika Hassinen <hassinen.mika@gmail.com>
2016-03-04 21:48:44 +0200

    Update: added led level control and enabler pin for sensor

--- moisture-sensor.ino
+++ moisture-sensor.ino
@@ -1,24 +1,86 @@
-int sensorPin = A1;    // select the input pin for the potentiometer
+#define TIME_INTERVAL 1000
+#define ENABLER_PIN 2		// output pin that enables the sensor while reading the value
+#define SENSOR_PIN A1		// input pin for the sensor
+
+int ledPins[] = {3, 4, 5, 6, 7};
+
 int sensorValue = 0;  // variable to store the value coming from the sensor
  
-#define TIME_INTERVAL 1000
+int ledLevel = 0;
 
 long timer = 0;
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
-   Serial.begin(115200);  
+
+	// setup serial for debugging
+	Serial.begin(115200);
+
+	// setup outputs
+	pinMode(SENSOR_PIN, OUTPUT);
+
+	// test leds
+	for (int pin = 0; pin < (sizeof(ledPins)/sizeof(int)); pin++ ) {
+		pinMode(ledPins[pin], OUTPUT);
+		delay(150);
+		digitalWrite(ledPins[pin], HIGH);
+	}
+	delay(1000);
+	setLedLevel(0);
+	delay(100);
+
+	// test sensor
+	digitalWrite(ENABLER_PIN, HIGH);
+	sensorValue = analogRead(SENSOR_PIN);
+	Serial.print("[DEBUG]: test read value: ");
+	Serial.println(sensorValue);
+	digitalWrite(ENABLER_PIN, LOW);
 }
  
 void loop() {
-	unsigned long currentTime = millis();
-	if (currentTime - timer > TIME_INTERVAL) {
-		// read the value from the sensor:
-	  sensorValue = analogRead(sensorPin);
-	  Serial.print("sensor = " );              
-	  Serial.println(sensorValue);
 
+	
+	unsigned long currentTime = millis();
+	if ((unsigned long)(currentTime - timer) >= TIME_INTERVAL) {
+
+		// enable probe
+		digitalWrite(ENABLER_PIN, HIGH);
+		delay(10);
+		// read the value from the sensor:
+		sensorValue = analogRead(SENSOR_PIN);
+		// disable probe after reading values
+		digitalWrite(ENABLER_PIN, LOW);
+
+		
+
+		// breakpoints for different led levels:
+		if (sensorValue > 550) ledLevel = 5;
+		if (sensorValue < 550) ledLevel = 4;
+		if (sensorValue < 450) ledLevel = 3;
+		if (sensorValue < 350) ledLevel = 2;
+		if (sensorValue < 250) ledLevel = 1;		
+		setLedLevel(ledLevel);
+
+		// debug data
+		Serial.print("[DEBUG]: current value: ");
+		Serial.println(sensorValue);
+
+		// save time
 		timer = currentTime;
 	}
-                  
+									
+}
+
+void setLedLevel(int level) {
+	int ledArraySize = (sizeof(ledPins)/sizeof(int));
+	if (level < 0) level = 0;
+	if (level > (ledArraySize) )
+		level = ledArraySize;
+
+	for (int pin = 0; pin < ledArraySize; pin++ ) {
+		pinMode(ledPins[pin], OUTPUT);
+		if (level >= pin+1) 
+			digitalWrite(ledPins[pin], HIGH);
+		else
+			digitalWrite(ledPins[pin], LOW);
+	}
 }
